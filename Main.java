94e2872460522dc50e28239e2f0daa0625cbdfc3
Vincent Hourdel <85120340+SelinCAHA@users.noreply.github.com>
2021-06-21 12:12:47 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World !") ;
         System.out.println("Hello, Selin !") ;
-        System.out.println("Hello !") ;
         System.out.println("Hello final?") ;
         System.out.println("nooo") ;
         System.out.println("YEEEEES") ;
